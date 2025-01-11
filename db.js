2a417e34a50a924877d4272539ac7bf029dae000
Tallysson2k23 <tallysson.bldd@gmail.com>
2025-01-11 11:23:55 -0300

    instalação do firebasee
    npm install firebase

--- db.js
+++ db.js
@@ -2,12 +2,5 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBzfan0YmyXqZBta-fkopDjTa7z-zdB0NM",
-    authDomain: "reserva-de-carros.firebaseapp.com",
-    databaseURL: "https://reserva-de-carros-default-rtdb.firebaseio.com",
-    projectId: "reserva-de-carros",
-    storageBucket: "reserva-de-carros.firebasestorage.app",
-    messagingSenderId: "242198935354",
-    appId: "1:242198935354:web:3ad94cdfeffb1ccd28c203",
-    measurementId: "G-W9Z2XNEBKC"
+    
   };
