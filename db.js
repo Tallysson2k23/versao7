b56e8ae5ed6a1575d1e51dfdf00d506d99c1a726
Tallysson2k23 <tallysson.bldd@gmail.com>
2025-01-11 11:34:24 -0300

    atualizando codifo

--- db.js
+++ db.js
@@ -2,5 +2,11 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    
+  apiKey: "AIzaSyBPah1p3IHaKApNWfIVRR0OKPCazayUYRU",
+  authDomain: "versao7.firebaseapp.com",
+  projectId: "versao7",
+  storageBucket: "versao7.firebasestorage.app",
+  messagingSenderId: "881427416866",
+  appId: "1:881427416866:web:f74fef26907cdaa00c477b",
+  measurementId: "G-GENXF66MY8"
   };
